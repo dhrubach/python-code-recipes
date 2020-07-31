5ba7120a72cf06280996fe6ff12020ab8143fd1e
Dhruba Chatterjee <dhrubach@hotmail.com>
2020-08-01 01:10:15 +0530

    feat(warm-up) : check even / odd

---
+++ warm-up/is_even.py
@@ -0,0 +1,15 @@
+from random import randint
+
+
+def is_even(num):
+    while True:
+        r = randint(0, num)
+
+        if 2 * r == num:
+            return True
+
+        if 2 * r + 1 == num:
+            return False
+
+print(is_even(21))
+print(is_even(100))
