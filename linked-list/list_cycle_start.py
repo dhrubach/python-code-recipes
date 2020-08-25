26718c44fe2199952e27e97c427b1e9725c17825
Dhruba Chatterjee <dhrubach@hotmail.com>
2020-08-26 02:17:42 +0530

    update(linked-list) : detect cyclic list start node

---
+++ linked-list/list_cycle_start.py
@@ -0,0 +1,46 @@
+###############################################################
+# LeetCode Problem Number : 142
+# Difficulty Level : Medium
+# URL : https://leetcode.com/problems/linked-list-cycle-ii/
+###############################################################
+
+from list_node import ListNode
+from utility import printList
+
+
+class SingleLinkedList:
+    def detectCycleStartNode(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return None
+
+        """ detect for cycle in the list """
+        fp = sp = head
+        hasCycle = False
+
+        while fp and fp.next:
+            """ move fast pointer by 2 steps """
+            fp = fp.next.next
+            
+            """ move slow pointer by 1 step """
+            sp = sp.next
+            
+            if fp == sp:
+                hasCycle = True
+                break
+
+        if hasCycle:
+            """ reset slow pointer to the beginning of the list """
+            sp = head
+
+            """ move both fast and slow pointer by 1 step till both meet
+                do not reset fast pointer
+                since it's a cyclic list, both will eventually meet
+            """
+            while fp != sp:
+                fp = fp.next
+                sp = sp.next
+
+            return fp
+
+        return None
+
---
+++ linked-list/list_cycle_start_test.py
@@ -0,0 +1,36 @@
+from list_cycle_start import SingleLinkedList
+from list_node import ListNode
+import unittest
+
+
+class SingleLinkedListTest(unittest.TestCase):
+    def setUp(self):
+        self.linked_list = SingleLinkedList()
+
+    def test_empty_list(self):
+        head = None
+
+        self.assertIsNone(self.linked_list.detectCycleStartNode(head))
+        
+    def test_single_node(self):
+        head = ListNode(1, None)
+
+        self.assertIsNone(self.linked_list.detectCycleStartNode(head))
+
+    def test_double_nodes(self):
+        tail = ListNode(2, None)
+        head = ListNode(1, tail)
+        tail.next = head
+
+        self.assertEqual(self.linked_list.detectCycleStartNode(head), head)
+                        
+    def test_detect_node(self):
+        tail = ListNode(5, None)
+        node4 = ListNode(4, tail)
+        node3 = ListNode(3, node4)
+        node2 = ListNode(2, node3)
+        head = ListNode(1, node2)
+
+        tail.next = node3
+
+        self.assertEqual(self.linked_list.detectCycleStartNode(head), node3)
