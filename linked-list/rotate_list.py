ad4ba4763d84481c6730ae5b59233781adff0940
Dhruba Chatterjee <dhrubach@hotmail.com>
2020-08-28 00:58:30 +0530

    update(linked-list) : rotate list

---
+++ linked-list/rotate_list.py
@@ -0,0 +1,54 @@
+##################################################
+# LeetCode Problem Number : 61
+# Difficulty Level : Medium
+# URL : https://leetcode.com/problems/rotate-list/
+##################################################
+from list_node import ListNode
+
+
+class RotateList:
+    def rotate(self, head: ListNode, k: int) -> ListNode:
+        if not head or not head.next:
+            return head
+
+        tail = head
+        length = 1
+
+        """ find the length of the list and it's tail
+        """
+        while tail.next:
+            tail = tail.next
+            length = length + 1
+
+        """ link the tail to its head making it a circular list
+            example input : 1 -> 2 -> 3 -> 4 -> 5 -> None
+            post linking : 1 -> 2 -> 3 -> 4 -> 5 -> back to head
+        """
+        tail.next = head
+
+        """ calculate the number of steps to reach the node which will
+            be the new tail after rotation
+            
+            if k is a multiple of length, then the list will remain as is
+        """
+        k = k % length
+
+        """ move the tail to the new tail node 
+            example : 
+                1 -> 2 -> 3 -> 4 -> 5 -> back to head
+                length 5
+                k = 2
+                
+                3 steps are required to reach 3
+        """
+        for _ in range(length - k):
+            tail = tail.next
+
+        """ set ListNode(4) to be the new head
+            set ListNode(3) to be the new tail
+        """
+        head = tail.next
+        tail.next = None
+
+        return head
+
---
+++ linked-list/rotate_list_test.py
@@ -0,0 +1,38 @@
+from rotate_list import RotateList
+from list_node import ListNode
+from utility import printList
+
+import unittest
+
+
+class RotateListTest(unittest.TestCase):
+    def setUp(self):
+        self.linked_list = RotateList()
+
+    def test_single_node(self):
+        l = ListNode()
+
+        self.assertEqual(self.linked_list.rotate(l, 2), l)
+
+    def test_double_node(self):
+        tail = ListNode(2, None)
+        head = ListNode(1, tail)
+
+        self.assertEqual(self.linked_list.rotate(head, 4), head)
+
+    def test_rotate_nodes(self):
+        tail = ListNode(5, None)
+        l = ListNode(4, tail)
+        l = ListNode(3, l)
+        l = ListNode(2, l)
+        l = ListNode(1, l)
+
+        rlist = self.linked_list.rotate(l, 2)
+        self.assertEqual(rlist.val, 4)
+
+        tail = ListNode(2, None)
+        l = ListNode(1, tail)
+        l = ListNode(0, l)
+
+        rlist = self.linked_list.rotate(l, 4)
+        self.assertEqual(rlist.val, 2)
