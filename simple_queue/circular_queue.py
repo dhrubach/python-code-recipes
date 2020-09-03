2ef02086b4e9342198f3b6fdb12a1f132421639f
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2020-09-03 19:23:57 +0530

    update(queue) : circular queue + unit tests

--- simple_queue/circular_queue.py
+++ simple_queue/circular_queue.py
@@ -1,31 +1,117 @@
+###############################################################
+# LeetCode Problem Number : 622
+# Difficulty Level : Medium
+# URL : https://leetcode.com/problems/design-circular-queue/
+###############################################################
 class CircularQueue:
     def __init__(self, size):
+        """initialize your data structure here"""
         self.max_size = size
-        self.head = 0
-        self.tail = 0
-        self.data = []
+
+        """ set head and tail pointer to -1 """
+        self.head = -1
+        self.tail = -1
+
+        """ initialize internal array """
+        self.data = [[] for _ in range(size)]
 
     def isEmpty(self) -> bool:
-        return len(self.data) == 0
+        """checks whether the circular queue is empty or not"""
+        return self.computeSize() == 0
 
     def isFull(self) -> bool:
-        return len(self.data) == self.max_size
+        """checks whether the circular queue is full or not"""
+        return self.computeSize() == self.max_size
 
     def enqueue(self, val: int) -> bool:
+        """insert an element into the circular queue
+        return true if the operation is successful
+        """
         if self.isFull():
             return False
 
-        self.data[self.tail] = val
+        """ move tail pointer to the next valid index
+            example : max_size : 5, tail : 2, next tail : 3
+                      max_size : 5, tail : 4, next tail : 0
+        """
         self.tail = (self.tail + 1) % self.max_size
+        self.data[self.tail] = val
+
+        """ for the first enqueue operation, set head to point to tail """
+        if self.head == -1:
+            self.head = self.tail
 
         return True
 
     def dequeue(self) -> bool:
+        """delete an element from the circular queue
+        return true if the operation is successful
+        """
         if self.isEmpty():
             return False
 
         self.data[self.head] = None
-        self.head = (self.head + 1) % self.max_size
+
+        """ if empty queue, set head to -1
+            else move head to the next valid insert position
+        """
+        self.head = -1 if self.computeSize() == 1 else (self.head + 1) % self.max_size
+
+        """ reset tail pointer for an empty queue """
+        if self.head == -1:
+            self.tail = -1
 
         return True
 
+    def front(self) -> int:
+        """get the front item from the queue"""
+        if self.isEmpty():
+            return -1
+
+        """ return value pointed by head pointer """
+        return self.data[self.head]
+
+    def rear(self) -> int:
+        """get the last item from the queue"""
+        if self.isEmpty():
+            return -1
+
+        """ return value pointed by tail pointer """
+        return self.data[self.tail]
+
+    def computeSize(self) -> int:
+        """ queue is empty if head pointer is set to -1 """
+        if self.head == -1:
+            return 0
+
+        """ if both head and tail pointers are set to the same index,
+            then queue has only 1 item
+          """
+        if self.tail == self.head:
+            return 1
+
+        """ if tail points to an index after head, then current size : (tail - head) + 1
+            if tail points to an index before head, then current size : (tail - head) + 1 + max-size
+        """
+        diff = self.tail - self.head
+        return diff + 1 if diff > 0 else diff + 1 + self.max_size
+
+    def executeCommands(self, command: str, operands: list):
+        """Automate execution of large number of operations to test implementation.
+
+        Not part of main queue implementation.
+        """
+        if command == "enQueue":
+            return self.enqueue(operands[0])
+        elif command == "deQueue":
+            return self.dequeue()
+        elif command == "Rear":
+            return self.rear()
+        elif command == "Front":
+            return self.front()
+        elif command == "isFull":
+            return self.isFull()
+        elif command == "isEmpty":
+            return self.isEmpty()
+
+        return
---
+++ simple_queue/tests/circular_queue_test.py
@@ -0,0 +1,85 @@
+from simple_queue.circular_queue import CircularQueue
+
+import pytest
+
+
+class TestCircularQueue:
+    def test_is_empty(self):
+        cq = CircularQueue(3)
+        assert cq.isEmpty() == True
+
+        cq.enqueue(1)
+        assert cq.isEmpty() == False
+
+        cq.dequeue()
+        assert cq.isEmpty() == True
+
+    def test_is_full(self):
+        cq = CircularQueue(3)
+
+        cq.enqueue(1)
+        cq.enqueue(2)
+        cq.enqueue(3)
+        assert cq.isFull() == True
+
+        cq.dequeue()
+        assert cq.isFull() == False
+
+    def test_is_front(self):
+        cq = CircularQueue(3)
+
+        cq.enqueue(1)
+        cq.enqueue(2)
+        cq.enqueue(3)
+
+        assert cq.front() == 1
+
+    def test_is_rear(self):
+        cq = CircularQueue(3)
+
+        cq.enqueue(1)
+        cq.enqueue(2)
+        cq.enqueue(3)
+
+        assert cq.rear() == 3
+
+    def test_sample_case(self):
+        cq = CircularQueue(3)
+
+        cq.enqueue(1)
+        cq.enqueue(2)
+        cq.enqueue(3)
+
+        assert cq.enqueue(4) == False
+        assert cq.rear() == 3
+        assert cq.isFull() == True
+
+        cq.dequeue()
+        cq.enqueue(4)
+
+        assert cq.rear() == 4
+
+    def test_sample_case_2(self):
+        cq = CircularQueue(6)
+
+        commands = [
+            "enQueue",
+            "Rear",
+            "Rear",
+            "deQueue",
+            "enQueue",
+            "Rear",
+            "deQueue",
+            "Front",
+            "deQueue",
+            "deQueue",
+            "deQueue",
+        ]
+        inputParams = [[6], [], [], [], [5], [], [], [], [], [], []]
+        result = []
+        expected_result = [True, 6, 6, True, True, 5, True, -1, False, False, False]
+
+        for c, p in zip(commands, inputParams):
+            result.append(cq.executeCommands(c, p))
+
+        assert result == expected_result
