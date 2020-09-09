706ad91bb45fe8d3a38bfe99d2dc147ea2afde0b
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2020-09-10 00:28:02 +0530

    update(binary-tree) : n-ary pre-order traversal

---
+++ binary_tree/nary_pre_traversal.py
@@ -0,0 +1,46 @@
+from binary_tree.node import Node
+
+
+class PreorderTreeTraversal:
+    def traverse(self, root: Node) -> [int]:
+        if not root:
+            return []
+
+        """ create an empty list to record elements under a sub-tree """
+        res = []
+
+        """ insert root node of current sub-tree """
+        res.append(root.val)
+
+        """ check for leaf node """
+        if root.children:
+            for ch in root.children:
+                """ for each child node, traverse its sub-tree """
+                res += self.traverse(ch)
+
+        """ return traversed nodes of a sub-tree """
+        return res
+
+    def traverse_iterative(self, root: Node) -> [int]:
+        if not root:
+            return []
+
+        res = []
+        """ start iteration by inserting root node into the stack """
+        stack = [root]
+
+        while stack:
+            node: Node = stack.pop()
+            res.append(node.val)
+
+            if node.children:
+                """ reverse the order of children to iterate from left to right
+
+                    without it, last children in a level will be popped out of
+                    the stack first in line 33 above resulting in right -> left
+                    traversal
+                """
+                for ch in reversed(node.children):
+                    stack.append(ch)
+
+        return res
---
+++ binary_tree/tests/nary_pre_traversal_test.py
@@ -0,0 +1,56 @@
+from binary_tree.nary_pre_traversal import PreorderTreeTraversal
+from binary_tree.node import Node
+
+
+class TestPreorderTreeTraversal:
+    def test_traversal_1(self):
+        pr = PreorderTreeTraversal()
+        rt = Node(1, children=[Node(3, children=[Node(5), Node(6)]), Node(2), Node(4)])
+
+        ans = pr.traverse(rt)
+
+        assert ans == [1, 3, 5, 6, 2, 4]
+
+    def test_traversal_2(self):
+        pr = PreorderTreeTraversal()
+        rt = Node(
+            1,
+            children=[
+                Node(2),
+                Node(
+                    3,
+                    children=[
+                        Node(6),
+                        Node(7, children=[Node(11, children=[Node(14)])]),
+                    ],
+                ),
+                Node(4, children=[Node(8, children=[Node(12)])]),
+                Node(5, children=[Node(9, children=[Node(13)]), Node(10)]),
+            ],
+        )
+
+        ans = pr.traverse(rt)
+
+        assert ans == [1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10]
+
+    def test_iterative_traversal_1(self):
+        pr = PreorderTreeTraversal()
+        rt = Node(
+            10,
+            children=[
+                Node(11),
+                Node(
+                    12,
+                    children=[
+                        Node(15),
+                        Node(16, children=[Node(17, children=[Node(20)])]),
+                    ],
+                ),
+                Node(13, children=[Node(18, children=[Node(21)])]),
+                Node(14, children=[Node(19, children=[Node(22)]), Node(23)]),
+            ],
+        )
+
+        ans = pr.traverse_iterative(rt)
+
+        assert ans == [10, 11, 12, 15, 16, 17, 20, 13, 18, 21, 14, 19, 22, 23]
