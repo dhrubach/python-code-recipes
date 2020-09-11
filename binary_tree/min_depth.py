042a5a446f9d779ceb3123891b195ef7fbad2de1
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2020-09-11 11:27:07 +0530

    update(binary-tree) : minimum depth - bfs

--- binary_tree/min_depth.py
+++ binary_tree/min_depth.py
@@ -5,10 +5,12 @@
 #####################################################################
 
 from binary_search_tree.tree_node import TreeNode
+from collections import deque
 
 
 class BinaryTree:
-    # DFS
+
+    # DFS -> runtime 40ms, memory 15.8MB
     def minDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
@@ -18,16 +20,53 @@ class BinaryTree:
             return 1
         elif not root.left:
             """if left child is not present, then calculate depth based only on right child
+
             add 1 for the current node
             """
             return 1 + self.minDepth(root.right)
         elif not root.right:
             """if right child is not present, then calculate depth based only on left child
+
             add 1 for the current node
             """
             return 1 + self.minDepth(root.left)
         else:
             """calculate depth of each child and return minimum of the two
+
             add 1 for the current node
             """
             return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+    # BFS -> runtime 40ms, memory 14.8MB
+    def minDepth_bfs(self, root: TreeNode) -> int:
+        if root == None:
+            return 0
+
+        """ depth at root level -> 1 """
+        queue, level = deque([(root, 1)]), 0
+
+        """ remove a node from beginning of the queue
+            add a node to the end of the queue
+
+            all nodes at a given level are processed from
+            left to right in a single iteration
+        """
+        while queue:
+            node, level = queue.popleft()
+
+            """ leaf node at a given level will be the minimum depth
+            """
+            if not node.left and not node.right:
+                return level
+            if node.left:
+                """if left node is present, add to queue
+
+                increment level by 1 to reflect the child node level
+                """
+                queue.append((node.left, level + 1))
+            if node.right:
+                """if right node is present, add to queue
+
+                increment level by 1 to reflect the child node level
+                """
+                queue.append((node.right, level + 1))
--- binary_tree/tests/min_depth_test.py
+++ binary_tree/tests/min_depth_test.py
@@ -84,3 +84,63 @@ class TestMinDeptBinaryTree:
 
         ans = bt.minDepth(rt)
         assert ans == 3
+
+    def test_line_tree_bfs(self):
+        bt = BinaryTree()
+        nodes = [
+            1,
+            2,
+            None,
+            3,
+            None,
+            None,
+            None,
+            4,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        ]
+        rt = build(nodes)
+
+        ans = bt.minDepth_bfs(rt)
+        assert ans == 4
+
+        nodes = [
+            1,
+            None,
+            2,
+            None,
+            None,
+            None,
+            3,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            4,
+        ]
+        rt = build(nodes)
+
+        ans = bt.minDepth_bfs(rt)
+        assert ans == 4
+
+    def test_lc_data_bfs(self):
+        bt = BinaryTree()
+        nodes = [3, 9, 20, None, None, 15, 7]
+        rt = build(nodes)
+
+        ans = bt.minDepth_bfs(rt)
+        assert ans == 2
+
+        nodes = [1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13]
+        rt = build(nodes)
+
+        ans = bt.minDepth_bfs(rt)
+        assert ans == 3
