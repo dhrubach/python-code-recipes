1247f255f67dc6e941ca2f3fd1c4feb63011d43a
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2020-09-11 10:59:29 +0530

    update(binary-tree) : minimum depth - dfs - optimize

--- binary_tree/min_depth.py
+++ binary_tree/min_depth.py
@@ -1,28 +1,33 @@
+#####################################################################
+# LeetCode Problem Number : 111
+# Difficulty Level : Easy
+# URL : https://leetcode.com/problems/minimum-depth-of-binary-tree/
+#####################################################################
+
 from binary_search_tree.tree_node import TreeNode
 
 
 class BinaryTree:
+    # DFS
     def minDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
 
         if not root.left and not root.right:
+            """ return 1 for leaf node """
             return 1
-
-        return self.calculateDepth(root, 0)
-
-    def calculateDepth(self, node: TreeNode, depth: int) -> int:
-        if not node:
-            return depth
-
-        depth += 1
-
-        ldepth = self.calculateDepth(node.left, 0) if node.left else None
-        rdepth = self.calculateDepth(node.right, 0) if node.right else None
-
-        if ldepth and rdepth:
-            return depth + (ldepth if ldepth <= rdepth else rdepth)
-        elif ldepth or rdepth:
-            return depth + (ldepth or rdepth)
+        elif not root.left:
+            """if left child is not present, then calculate depth based only on right child
+            add 1 for the current node
+            """
+            return 1 + self.minDepth(root.right)
+        elif not root.right:
+            """if right child is not present, then calculate depth based only on left child
+            add 1 for the current node
+            """
+            return 1 + self.minDepth(root.left)
         else:
-            return depth
+            """calculate depth of each child and return minimum of the two
+            add 1 for the current node
+            """
+            return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
