27490a2c653242213e8c2d0a9335f80e336b591d
Dhruba Chatterjee <dhrubach@hotmail.com>
2020-09-13 01:28:36 +0530

    update(array) : third largest element

---
+++ simple_array/tests/third_largest_test.py
@@ -0,0 +1,21 @@
+from simple_array.third_largest import ThirdLargestNumber
+
+
+class TestThirdLargestNumber:
+    def test_two_element(self):
+        th = ThirdLargestNumber()
+        ans = th.find([3, 4])
+
+        assert ans == 4
+
+    def test_duplicate_ele(self):
+        th = ThirdLargestNumber()
+        ans = th.find([3, 4, 2, 2, 1])
+
+        assert ans == 2
+
+    def test_lc_data(self):
+        th = ThirdLargestNumber()
+        ans = th.find([1, 4, 5, 2, 7, 8, 9, 3, 4])
+
+        assert ans == 7
---
+++ simple_array/third_largest.py
@@ -0,0 +1,26 @@
+############################################################
+# LeetCode Problem Number : 414
+# Difficulty Level : Easy
+# URL : https://leetcode.com/problems/third-maximum-number/
+############################################################
+
+
+class ThirdLargestNumber:
+    # runtime -> 97.90%, memory -> 83.47%
+    # complexity -> time O(n), space O(1)
+    def find(self, nums: [int]) -> int:
+        if len(nums) < 3:
+            return max(nums)
+
+        temp_list = [float("-inf"), float("-inf"), float("-inf")]
+
+        for num in nums:
+            if num not in temp_list:
+                if num > temp_list[0]:
+                    temp_list = [num, temp_list[0], temp_list[1]]
+                elif num > temp_list[1]:
+                    temp_list = [temp_list[0], num, temp_list[1]]
+                elif num > temp_list[2]:
+                    temp_list = [temp_list[0], temp_list[1], num]
+
+        return max(temp_list) if float("-inf") in temp_list else temp_list[2]
