d6359c9c5554411b718d6fe810ac80bab727ec99
Dhruba Chatterjee <dhruba.chatterjee@fiserv.com>
2020-10-14 15:16:16 +0530

    update(dfs) : number of islands - bfs

--- depth_first_search/m_number_of_islands.py
+++ depth_first_search/m_number_of_islands.py
@@ -3,24 +3,31 @@
 # Difficulty Level : Medium
 # URL : https://leetcode.com/problems/number-of-islands/
 ########################################################
-from typing import List
+from typing import List, Deque
+from collections import deque
 
 
 class NumberOfIslands:
-    def calculate(self, grid: List[List[int]]) -> int:
+    def calculate(self, grid: List[List[int]], type: str) -> int:
         if not grid:
             return 0
 
         count = 0
+        queue = deque([])
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    """start a search once you encounter a '1'
+                    if type == "dfs":
+                        """start a search once you encounter a '1'
 
-                    increment counter by 1 upon return
-                    """
-                    self.dfs_search(grid, i, j)
-                    count += 1
+                        increment counter by 1 upon return
+                        """
+                        self.dfs_search(grid, i, j)
+                        count += 1
+                    elif type == "bfs":
+                        queue.append((i, j))
+                        self.bfs_search(grid, queue)
+                        count += 1
 
         return count
 
@@ -40,3 +47,18 @@ class NumberOfIslands:
         self.dfs_search(grid, i - 1, j)
         self.dfs_search(grid, i, j + 1)
         self.dfs_search(grid, i, j - 1)
+
+    def bfs_search(self, grid: List[List[int]], queue: Deque) -> None:
+        while queue:
+            i, j = queue.popleft()
+            grid[i][j] = "#"
+
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if (
+                    x >= 0
+                    and x < len(grid)
+                    and y >= 0
+                    and y < len(grid[0])
+                    and grid[x][y] == 1
+                ):
+                    queue.append((x, y))
--- depth_first_search/tests/m_number_of_islands_test.py
+++ depth_first_search/tests/m_number_of_islands_test.py
@@ -4,42 +4,66 @@ from depth_first_search.m_number_of_islands import NumberOfIslands
 class TestNumberOfIslands:
     def test_empty_grid(self):
         nm = NumberOfIslands()
-        ans = nm.calculate(None)
+        ans = nm.calculate(None, "dfs")
 
         assert ans == 0
 
     def test_single_island(self):
         nm = NumberOfIslands()
-        ans = nm.calculate([[1]])
 
+        ans = nm.calculate([[1]], "dfs")
+        assert ans == 1
+
+        ans = nm.calculate([[1]], "bfs")
         assert ans == 1
 
     def test_no_island(self):
         nm = NumberOfIslands()
-        ans = nm.calculate([[0]])
 
+        ans = nm.calculate([[0]], "dfs")
+        assert ans == 0
+
+        ans = nm.calculate([[0]], "bfs")
         assert ans == 0
 
     def test_lc_input_1(self):
         nm = NumberOfIslands()
+
         grid = [
             [1, 1, 1, 1, 0],
             [1, 1, 0, 1, 0],
             [1, 1, 0, 0, 0],
             [0, 0, 0, 0, 0],
         ]
-        ans = nm.calculate(grid)
+        ans = nm.calculate(grid, "dfs")
+        assert ans == 1
 
+        grid = [
+            [1, 1, 1, 1, 0],
+            [1, 1, 0, 1, 0],
+            [1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+        ans = nm.calculate(grid, "bfs")
         assert ans == 1
 
     def test_lc_input_2(self):
         nm = NumberOfIslands()
+
         grid = [
             [1, 1, 0, 0, 0],
             [1, 1, 0, 0, 0],
             [0, 0, 1, 0, 0],
             [0, 0, 0, 1, 1],
         ]
-        ans = nm.calculate(grid)
+        ans = nm.calculate(grid, "dfs")
+        assert ans == 3
 
+        grid = [
+            [1, 1, 0, 0, 0],
+            [1, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 1],
+        ]
+        ans = nm.calculate(grid, "bfs")
         assert ans == 3
